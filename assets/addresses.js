298d9b1da50e93d5aee1449054cf95b17cfbaf72
farhantanvirtushar <farhantanvir.2340.csedu@gmail.com>
2023-09-28 13:59:31 +0600

    Add addresses page & add new address create form

---
+++ assets/addresses.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#new-address").modal("hide");
+
+    $("#add-new-address").on("click", function(){
+        $("#new-address").modal("show");
+    })
+  });
+  
